9c53f5864fc00aee9ff5639fc27e79409b7a4220
Ferdinand Prantl <prantlf@gmail.com>
2022-06-18 10:13:12 +0200

    fix: Omit format from esbuild as long as possible
    The format surprisingly affects the minified output. Setting it to undefined
    retains the functionality of the original code the best. However CJS output
    needs the format set to "cjs", otherwise the module-local identifiers will
    not be minified.

--- test/index.js
+++ test/index.js
@@ -21,15 +21,52 @@ test.before(async () => {
   bundle = await rollup({ input: 'test/entry.js' })
 })
 
-test('minify without source map', async () => {
+test('minify iife', async () => {
   await bundle.write({
     plugins: [minify()],
     file: 'test/out.js',
+    strict: false,
+    format: 'iife',
+    name: 'glob'
+  })
+
+  const code = await readFile('test/out.js', 'utf8')
+  strictEqual(code, 'var glob=function(e){var l=()=>{({}!==void 0)&&console.log("glob")};function o(){console.log("help")}return e.glob=l,e.help=o,Object.defineProperty(e,"__esModule",{value:!0}),e}({});\n')
+})
+
+test('minify umd', async () => {
+  await bundle.write({
+    plugins: [minify()],
+    file: 'test/out.js',
+    strict: false,
+    format: 'umd',
+    name: 'glob'
+  })
+
+  const code = await readFile('test/out.js', 'utf8')
+  strictEqual(code, '(function(e,o){typeof exports=="object"&&typeof module<"u"?o(exports):typeof define=="function"&&define.amd?define(["exports"],o):(e=typeof globalThis<"u"?globalThis:e||self,o(e.glob={}))})(this,function(e){var o=()=>{({}!==void 0)&&console.log("glob")};function n(){console.log("help")}e.glob=o,e.help=n,Object.defineProperty(e,"__esModule",{value:!0})});\n')
+})
+
+test('minify cjs', async () => {
+  await bundle.write({
+    plugins: [minify()],
+    file: 'test/out.js',
+    strict: false,
     format: 'cjs'
   })
 
   const code = await readFile('test/out.js', 'utf8')
-  strictEqual(code, '"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=()=>{({}!==void 0)&&console.log("glob")};function l(){console.log("help")}exports.glob=e,exports.help=l;\n')
+  strictEqual(code, 'Object.defineProperty(exports,"__esModule",{value:!0});var e=()=>{({}!==void 0)&&console.log("glob")};function l(){console.log("help")}exports.glob=e,exports.help=l;\n')
+})
+
+test('minify es', async () => {
+  await bundle.write({
+    plugins: [minify()],
+    file: 'test/out.js'
+  })
+
+  const code = await readFile('test/out.js', 'utf8')
+  strictEqual(code, 'var o=()=>{({}!==void 0)&&console.log("glob")};function l(){console.log("help")}export{o as glob,l as help};\n')
 })
 
 test('minify with source map', async () => {
